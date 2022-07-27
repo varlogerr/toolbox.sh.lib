a60b6d14987d726efd0d460e14eeea8e0f5c14ab
Varlog Err <varlog.err@gmail.com>
2022-07-27 19:29:54 +0300

    WiP

--- tests/suit.channel/test.sh
+++ tests/suit.channel/test.sh
@@ -3,23 +3,45 @@ __channel() {
 
   local out_exp
   local out_act
+  local input
 
   {
     out_act="$(shlib_channel1_print)"
     assert_result "1" "$?" "" "${out_act}" \
       "channel1_print: RC 1 and blank stdout on no messages"
 
-    shlib_channel1_print
-    assert_result "1" "$?" "" "${SHLIB_CHANNEL1}" \
-      "channel1_print: RC 1 and blank SHLIB_CHANNEL1 on no messages"
-
     out_act="$(shlib_channel2_print 2>&1 1>/dev/null)"
     assert_result "1" "$?" "" "${out_act}" \
       "channel2_print: RC 1 and blank stderr on no messages"
 
+    shlib_channel1_print
+    assert_result "1" "$?" "" "${SHLIB_CHANNEL1}" \
+      "channel1_print: RC 1 and blank SHLIB_CHANNEL1 on no messages"
+
     shlib_channel2_print
     assert_result "1" "$?" "" "${SHLIB_CHANNEL2}" \
       "channel2_print: RC 1 and blank SHLIB_CHANNEL2 on no messages"
+  }
+
+  {
+    out_exp="one"$'\n'"two"
+    input=("one" "two")
+
+    out_act="$(shlib_channel1_print "${input[@]}")"
+    assert_result "0" "$?" "${out_exp}" "${out_act}" \
+      "channel1_print: Print multi messages to stdout"
+
+    out_act="$(shlib_channel2_print "${input[@]}" 2>&1 1>/dev/null)"
+    assert_result "0" "$?" "${out_exp}" "${out_act}" \
+      "channel1_print: Print multi messages to stderr"
+
+    shlib_channel1_print "${input[@]}" >/dev/null
+    assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL1}" \
+      "channel1_print: SHLIB_CHANNEL1 contains multi messages"
+
+    shlib_channel2_print "${input[@]}" 2>/dev/null
+    assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL2}" \
+      "channel1_print: SHLIB_CHANNEL2 contains multi messages"
   }
 
   for f in -q --quiet; do
@@ -41,4 +63,5 @@ __channel() {
     assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL2}" \
       "channel2_print: SHLIB_CHANNEL2 contains message on ${f} flag"
   done
+
 } && __channel
