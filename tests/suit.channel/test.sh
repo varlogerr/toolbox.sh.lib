499708c3c10b9b2404427211cd277ea372567d1a
Varlog Err <varlog.err@gmail.com>
2022-07-27 19:00:44 +0300

    WiP

--- tests/suit.channel/test.sh
+++ tests/suit.channel/test.sh
@@ -7,21 +7,21 @@ __channel() {
   {
     out_act="$(shlib_channel1_print)"
     assert_result "1" "$?" "" "${out_act}" \
-      "print: RC 1 and blank stdout on no messages"
+      "channel1_print: RC 1 and blank stdout on no messages"
 
     shlib_channel1_print
     assert_result "1" "$?" "" "${SHLIB_CHANNEL1}" \
-      "print: RC 1 and blank SHLIB_CHANNEL1 on no messages"
+      "channel1_print: RC 1 and blank SHLIB_CHANNEL1 on no messages"
   }
 
   for f in -q --quiet; do
     out_act="$(shlib_channel1_print "test" -q)"
     assert_result "0" "$?" "" "${out_act}" \
-      "print: RC 0 and blank stdout on ${f} flag"
+      "channel1_print: RC 0 and blank stdout on ${f} flag"
 
     out_exp="test"
     shlib_channel1_print "test" -q
     assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL1}" \
-      "print: RC 0 and SHLIB_CHANNEL1 contains message on ${f} flag"
+      "channel1_print: RC 0 and SHLIB_CHANNEL1 contains message on ${f} flag"
   done
 } && __channel
