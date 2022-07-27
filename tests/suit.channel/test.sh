67268e7f18c42667eba8676f1760cbebe22c632f
Varlog Err <varlog.err@gmail.com>
2022-07-27 19:06:04 +0300

    WiP

--- tests/suit.channel/test.sh
+++ tests/suit.channel/test.sh
@@ -12,6 +12,14 @@ __channel() {
     shlib_channel1_print
     assert_result "1" "$?" "" "${SHLIB_CHANNEL1}" \
       "channel1_print: RC 1 and blank SHLIB_CHANNEL1 on no messages"
+
+    out_act="$(shlib_channel2_print 2>&1 1>/dev/null)"
+    assert_result "1" "$?" "" "${out_act}" \
+      "channel2_print: RC 1 and blank stderr on no messages"
+
+    shlib_channel2_print
+    assert_result "1" "$?" "" "${SHLIB_CHANNEL2}" \
+      "channel2_print: RC 1 and blank SHLIB_CHANNEL2 on no messages"
   }
 
   for f in -q --quiet; do
@@ -23,5 +31,14 @@ __channel() {
     shlib_channel1_print "test" -q
     assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL1}" \
       "channel1_print: RC 0 and SHLIB_CHANNEL1 contains message on ${f} flag"
+
+    out_act="$(shlib_channel2_print "test" -q 2>&1 1>/dev/null)"
+    assert_result "0" "$?" "" "${out_act}" \
+      "channel2_print: RC 0 and blank stdout on ${f} flag"
+
+    out_exp="test"
+    shlib_channel2_print "test" -q
+    assert_result "0" "$?" "${out_exp}" "${SHLIB_CHANNEL2}" \
+      "channel2_print: RC 0 and SHLIB_CHANNEL2 contains message on ${f} flag"
   done
 } && __channel
