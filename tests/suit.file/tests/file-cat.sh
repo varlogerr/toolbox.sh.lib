608876e5aa4286718bddf72b009e180b111d004e
Varlog Err <varlog.err@gmail.com>
2022-07-27 10:48:40 +0300

    WiP

--- tests/suit.file/tests/file-cat.sh
+++ tests/suit.file/tests/file-cat.sh
@@ -7,7 +7,19 @@ __file_cat() {
     assert_result "${@:1:4}" "file_cat: ${5}"
   }
 
-  local cmd=shlib_file_cat
+  local -r cmd=shlib_file_cat
+  local -r RC_OK=0
+  local -r RC_ERR=1
+  declare -rA path=(
+    [file]="${FILES_DIR}/file.txt"
+    [lnk]="${FILES_DIR}/file.lnk"
+  )
+  declare -rA path_err=(
+    [file]="${FILES_DIR}/${GLOB_RANDVAL}.txt"
+    [lnk]="${FILES_DIR}/${GLOB_RANDVAL}.lnk"
+  )
+  local out_act
+  local out_exp
 
 
 } && __file_cat
