55a014a1a7f7cba91162e6b812277270bbb0acb0
Varlog Err <varlog.err@gmail.com>
2022-08-03 12:24:53 +0300

    WiP

--- tests/suit.list/test.sh
+++ tests/suit.list/test.sh
@@ -1,5 +1,6 @@
 local res_act
 local res_exp
+local input
 
 __test_from_args() {
   unset __test_from_args
@@ -29,12 +30,31 @@ __test_from_args() {
   {
     res_act="$(${cmd} "")"
     assert_from_args "${SHLIB_OK}" "${?}" "${res_exp}" "${res_act}" \
-      "single item list to stdout on empty string input"
+      "empty single item list to stdout on empty string input"
 
     unset retvar; declare retvar
     ${cmd} -r retvar "" >/dev/null
     assert_from_args "${SHLIB_OK}" "${?}" "${res_exp}" "${retvar}" \
-      "single item list to RETVAR on empty string input"
+      "empty single item list to RETVAR on empty string input"
+  }
+
+  res_exp="* one"$'\n'"* two"$'\n'"  "
+  input=("one" "two"$'\n')
+  unset retvars; declare -A retvars
+  local flagname
+  {
+    res_act="$(${cmd} "${input[@]}")"
+    assert_from_args "${SHLIB_OK}" "${?}" "${res_exp}" "${res_act}" \
+      "multi items with with single line to stdout"
+
+    for f in -r --retvar; do
+      ${cmd} "${input[@]}" ${f} retvars[$f] >/dev/null
+      assert_from_args "${SHLIB_OK}" "${?}" "${res_exp}" "${retvars[$f]}" \
+        "multi items with with single line to RETVAR: ${f}"
+    done
+
+    assert_from_args 0 0 "${retvars[-r]}" "${retvars[--retvar]}" \
+      "-r and --retvar flags provide same RETVAR value"
   }
 
   unset assert_from_args
